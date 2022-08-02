7f213d90570c0c224711f0d73140d0b32894f8a8
Deja Jackson <deja.jackson@dejajackson.me>
2022-08-02 14:11:43 -0700

    Initial Commit

---
+++ Peer2Power/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Peer2Power
+//
+//  Created by Deja Jackson on 8/2/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Peer2Power/Peer2PowerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Peer2PowerApp.swift
+//  Peer2Power
+//
+//  Created by Deja Jackson on 8/2/22.
+//
+
+import SwiftUI
+
+@main
+struct Peer2PowerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
