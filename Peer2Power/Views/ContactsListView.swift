d45967e0a84813e7ae130a7b50d9f4146f59a30d
Deja Jackson <deja.jackson@dejajackson.me>
2022-08-03 22:08:24 -0700

    Attempting to get contact list to work

--- Peer2Power/Views/ContactsListView.swift
+++ Peer2Power/Views/ContactsListView.swift
@@ -6,10 +6,49 @@
 //
 
 import SwiftUI
+import RealmSwift
 
 struct ContactsListView: View {
+    @AsyncOpen(appId: realmAppID, timeout: 4000) var asyncOpen
+    
+    @ObservedResults(Contact.self) var contacts
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        switch asyncOpen {
+        case .connecting:
+            ProgressView()
+        case .waitingForUser:
+            ProgressView("Waiting for user to log in...")
+        case .open(let realm):
+            List {
+                if contacts.isEmpty {
+                    Text("Add some contacts")
+                        .font(.subheadline)
+                        .foregroundColor(.gray)
+                }
+                ForEach(contacts) { contact in
+                    Text("\(contact.name)")
+                }
+            }
+            .onAppear {
+                let subscriptions = realm.subscriptions
+                subscriptions.update {
+                    if subscriptions.first(named: "user_contacts") != nil {
+                        print("Contact subscription already exists.")
+                        return
+                    } else {
+                        print("Appending new Contact subscription.")
+                        subscriptions.append(QuerySubscription<Contact>(name: "user_contacts") { contact in
+                            contact.owner_id == app.currentUser!.id
+                        })
+                    }
+                }
+            }
+        case .progress(let progress):
+            ProgressView(progress)
+        case .error(let error):
+            ErrorView(error: error, retryAction: {})
+        }
     }
 }
 
