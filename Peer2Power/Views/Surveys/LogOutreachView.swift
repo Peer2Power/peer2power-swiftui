c258b18161e11e033d199ed0af9685410fdc302b
Deja Jackson <deja.jackson@dejajackson.me>
2022-08-18 19:45:49 -0700

    Got survey to dismiss for any completion reason

--- Peer2Power/Views/Surveys/LogOutreachView.swift
+++ Peer2Power/Views/Surveys/LogOutreachView.swift
@@ -5,6 +5,7 @@
 //  Created by Deja Jackson on 8/18/22.
 //
 
+import Foundation
 import SwiftUI
 import UIKit
 import ResearchKit
@@ -14,12 +15,36 @@ struct LogOutreachView: UIViewControllerRepresentable {
         let task = LogOutreachTask(identifier: String(describing: Identifier.logAttemptQuestionTask), steps: [LogOutreachTask.howContactStep(), LogOutreachTask.volunteerInterestStep(), LogOutreachTask.volunteerStatusStep()])
         
         let taskViewController = ORKTaskViewController(task: task, taskRun: nil)
+        taskViewController.delegate = context.coordinator
         
         return taskViewController
     }
     
     func updateUIViewController(_ uiViewController: ORKTaskViewController, context: Context) {
         
+    }
+    
+    func makeCoordinator() -> Coordinator {
+        return Coordinator()
+    }
+    
+    class Coordinator: NSObject, ORKTaskViewControllerDelegate {
+        func taskViewController(_ taskViewController: ORKTaskViewController, didFinishWith reason: ORKTaskViewControllerFinishReason, error: Error?) {
+            switch reason {
+            case .saved:
+                print("Log attempt survey saved.")
+            case .discarded:
+                print("User discarded survey.")
+            case .completed:
+                print("Log attempt survey completed.")
+            case .failed:
+                print("Log attempt survey failed with the following error: \(error?.localizedDescription)")
+            @unknown default:
+                print("Unknown")
+            }
+            
+            taskViewController.dismiss(animated: true, completion: nil)
+        }
     }
 }
 
