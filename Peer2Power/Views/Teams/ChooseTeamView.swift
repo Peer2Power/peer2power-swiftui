8c0ae0dbdb619c968fd2ac5a774977126f681eeb
Deja Jackson <jacksodl@lafayette.edu>
2022-10-02 12:35:27 -0400

    Team detail screen will show the team's score and how many members they have

--- Peer2Power/Views/Teams/ChooseTeamView.swift
+++ Peer2Power/Views/Teams/ChooseTeamView.swift
@@ -23,9 +23,11 @@ struct ChooseTeamView: View {
             }
             
             if selectedParty != .selectParty {
-                VStack {
-                    Text("Score: \(teamScore ?? 0) points")
-                    Text("Members: \(teamMemberCount ?? 0)")
+                Section {
+                    VStack(alignment: .leading) {
+                        Text("Score: \(teamScore ?? 0) points")
+                        Text("Members: \(teamMemberCount ?? 0)")
+                    }
                 }
             }
         }
@@ -39,7 +41,48 @@ struct ChooseTeamView: View {
 
 extension ChooseTeamView {
     private func fetchTeamInfo() {
+        guard let url = URL(string: "\(mongoDataEndpoint)action/findOne") else { return }
         
+        var request = URLRequest(url: url)
+        
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.setValue(mongoDataAPIKey, forHTTPHeaderField: "api-key")
+        request.setValue("*", forHTTPHeaderField: "Access-Control-Request-Headers")
+        
+        let bodyJSON: [String: Any] = [
+            "collection": "Team",
+            "database": "peer2power",
+            "dataSource": "prod",
+            "filter": ["school_name": school_name, "party": selectedParty.rawValue],
+            "projection": ["_id": 0, "score": 1, "member_ids": 1]
+        ]
+        let bodyData = try? JSONSerialization.data(withJSONObject: bodyJSON)
+        
+        request.httpBody = bodyData
+        
+        request.httpMethod = "POST"
+        
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            guard let data = data, error == nil else {
+                print(error?.localizedDescription ?? "No data")
+                return
+            }
+            
+            let responseJSON = try? JSONSerialization.jsonObject(with: data)
+            guard let responseJSON = responseJSON as? [String: Any] else { return }
+            print(responseJSON)
+            
+            guard let team = responseJSON["document"] as? [String: Any] else { return }
+            
+            guard let score = team["score"] as? Int else { return }
+            teamScore = score
+            
+            //FIXME: figure out how member ids would be represented
+            guard let members = team["member_ids"] as? [String] else { return }
+            teamMemberCount = members.count
+        }
+        
+        task.resume()
     }
 }
 
