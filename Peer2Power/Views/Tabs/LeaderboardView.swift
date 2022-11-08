91d53d440535ce438ff1463d6710e7ab29ade2a6
Deja Jackson <jacksodl@lafayette.edu>
2022-11-08 17:55:33 -0500

    Added a graph to the leaderboard view for iOS 16 and above

--- Peer2Power/Views/Tabs/LeaderboardView.swift
+++ Peer2Power/Views/Tabs/LeaderboardView.swift
@@ -7,6 +7,7 @@
 
 import SwiftUI
 import RealmSwift
+import Charts
 
 struct LeaderboardView: View {
     @ObservedResults(Team.self,
@@ -38,49 +39,90 @@ struct LeaderboardView: View {
                     .multilineTextAlignment(.center)
             }
             Divider()
-            HStack {
-                VStack {
-                    Image("Democrats")
-                        .resizable()
-                        .renderingMode(.template)
-                        .aspectRatio(contentMode: .fit)
-                        .foregroundColor(Color("DemsColor"))
-                    Text("Democrats have")
-                        .font(.title3)
-                        .multilineTextAlignment(.center)
-                    Text("\(demTeams.sum(of: \Team.score))")
-                        .font(.title)
-                        .fontWeight(.bold)
-                        .multilineTextAlignment(.center)
-                    if demTeams.sum(of: \Team.score) != 1 {
-                        Text("points")
-                            .multilineTextAlignment(.center)
-                            .font(.title3)
-                    } else {
-                        Text("point")
-                            .multilineTextAlignment(.center)
-                            .font(.title3)
+            if #available(iOS 16.0, *) {
+                Chart {
+                    BarMark(
+                        x: .value("Party", "Democrats"),
+                        y: .value("Total Score", demTeams.sum(of: \Team.score))
+                    )
+                    .foregroundStyle(Color("DemsColor"))
+                    .annotation(position: .overlay, alignment: .center, spacing: nil) {
+                        VStack {
+                            Image("Democrats")
+                                .resizable()
+                                .renderingMode(.template)
+                                .aspectRatio(contentMode: .fit)
+                                .foregroundColor(.white)
+                            Text("\(demTeams.sum(of: \Team.score))")
+                                .foregroundColor(.white)
+                        }
+                        .padding(.horizontal, 15.0)
+                    }
+                    BarMark(
+                        x: .value("Party", "Republicans"),
+                        y: .value("Total Score", repTeams.sum(of: \Team.score))
+                    )
+                    .foregroundStyle(Color("GOPColor"))
+                    .annotation(position: .overlay, alignment: .center, spacing: nil) {
+                        VStack {
+                            Image("Republicans")
+                                .resizable()
+                                .renderingMode(.template)
+                                .aspectRatio(contentMode: .fit)
+                                .foregroundColor(.white)
+                            Text("\(repTeams.sum(of: \Team.score))")
+                                .foregroundColor(.white)
+                        }
+                        .padding(.horizontal, 15.0)
                     }
                 }
-                VStack {
-                    Image("Republicans")
-                        .resizable()
-                        .renderingMode(.template)
-                        .aspectRatio(contentMode: .fit)
-                        .foregroundColor(Color("GOPColor"))
-                    Text("Republicans have")
-                        .font(.title3)
-                    Text("\(repTeams.sum(of: \Team.score))")
-                        .font(.title)
-                        .fontWeight(.bold)
-                    if repTeams.sum(of: \Team.score) != 1 {
-                        Text("points")
-                            .multilineTextAlignment(.center)
+                .chartXScale(type: .category)
+                .chartYScale(domain: 0...demTeams.sum(of: \Team.score) + repTeams.sum(of: \Team.score))
+            } else {
+                HStack {
+                    VStack {
+                        Image("Democrats")
+                            .resizable()
+                            .renderingMode(.template)
+                            .aspectRatio(contentMode: .fit)
+                            .foregroundColor(Color("DemsColor"))
+                        Text("Democrats have")
                             .font(.title3)
-                    } else {
-                        Text("point")
                             .multilineTextAlignment(.center)
+                        Text("\(demTeams.sum(of: \Team.score))")
+                            .font(.title)
+                            .fontWeight(.bold)
+                            .multilineTextAlignment(.center)
+                        if demTeams.sum(of: \Team.score) != 1 {
+                            Text("points")
+                                .multilineTextAlignment(.center)
+                                .font(.title3)
+                        } else {
+                            Text("point")
+                                .multilineTextAlignment(.center)
+                                .font(.title3)
+                        }
+                    }
+                    VStack {
+                        Image("Republicans")
+                            .resizable()
+                            .renderingMode(.template)
+                            .aspectRatio(contentMode: .fit)
+                            .foregroundColor(Color("GOPColor"))
+                        Text("Republicans have")
                             .font(.title3)
+                        Text("\(repTeams.sum(of: \Team.score))")
+                            .font(.title)
+                            .fontWeight(.bold)
+                        if repTeams.sum(of: \Team.score) != 1 {
+                            Text("points")
+                                .multilineTextAlignment(.center)
+                                .font(.title3)
+                        } else {
+                            Text("point")
+                                .multilineTextAlignment(.center)
+                                .font(.title3)
+                        }
                     }
                 }
             }
