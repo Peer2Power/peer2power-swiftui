42f30afdac1eba2f71719502f88b3172b7f78492
Deja Jackson <jacksodl@lafayette.edu>
2023-01-15 11:18:12 -0500

    Updated dependencies

---
+++ Peer2Power/Views/SwitchTeamMenuView.swift
@@ -0,0 +1,22 @@
+//
+//  SwitchTeamMenuView.swift
+//  Peer2Power
+//
+//  Created by Deja Jackson on 1/13/23.
+//
+
+import SwiftUI
+
+struct SwitchTeamMenuView: View {
+    var body: some View {
+        Menu("Here's a menu") {
+            Button("Ay yo", action: {})
+        }
+    }
+}
+
+struct SwitchTeamMenuView_Previews: PreviewProvider {
+    static var previews: some View {
+        SwitchTeamMenuView()
+    }
+}
